b279093d5983ea14bd82e12d8699018e102a33d1
Naim Sulejmani <naim.sulejmani@gmail.com>
2024-11-08 20:13:44 +0100

    Setup Project

---
+++ src/main/java/dev/naimsulejmani/grupi3bookcheckmark/Grupi3BookCheckMarkApplication.java
@@ -0,0 +1,13 @@
+package dev.naimsulejmani.grupi3bookcheckmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupi3BookCheckMarkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Grupi3BookCheckMarkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/naimsulejmani/grupi3bookcheckmark/Grupi3BookCheckMarkApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.naimsulejmani.grupi3bookcheckmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupi3BookCheckMarkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
